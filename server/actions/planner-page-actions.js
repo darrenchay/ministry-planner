7c8f3a46d276d352a880164e10479afb52fd441a
darrenchay <darrenchay@gmail.com>
2021-07-27 23:12:55 -0400

    cleaning code

--- server/actions/planner-page-actions.js
+++ server/actions/planner-page-actions.js
@@ -22,34 +22,23 @@ class PlannerPageActions {
                         }
                     });
                 });
-                console.log("here after event function");
-                console.log(events);
+
                 var eventList = [];
                 var promises = []; //Will save the list of all promises to execute
 
                 // Adding event details to each event
                 for (var i = 0; i < events.length; i++) {
                     const event = events[i];
-                    // Creating promise for each event
+                    // Creating promise to find event details for each event
                     promises.push(new Promise((resolve, reject) => {
-                        console.log("Creating promise for:" + event.id);
                         worshipOnDutySchema.find({
                             eventId: event.id
                         }, function (err, worshipDetails) {
-                            if(worshipDetails != '') {
+                            if (worshipDetails != '') {
                                 var fullEvent = {}; //Creating a new variable to store the data
-                                // console.log("All details before adding them to custom object")
-                                // console.log("worship details: ");
-                                // console.log(worshipDetails);
-                                // console.log("event details: ");
-                                // console.log(tempEvent);
-                                // console.log("eventList before adding");
-                                // console.log(eventList);
 
                                 fullEvent.event = event;
                                 fullEvent.eventDetails = worshipDetails[0];
-                                // console.log("fullEvent before adding");
-                                // console.log(fullEvent);
                                 eventList.push(fullEvent);
                             } else {
                                 console.log("worship details not found");
@@ -57,20 +46,16 @@ class PlannerPageActions {
                             resolve();
                         });
                     }));
-                }          
-                Promise.all(promises).then(function() {
-                    console.log("EventList Data: ");
-                    console.log(eventList);
-                    res.status(200).send(eventList);    
+                }
+                Promise.all(promises).then(function () {
+                    res.status(200).send(eventList);
                 });
 
             } catch (err) {
                 console.log(err.details);
                 res.status(400).send(err);
             }
-            })();
-
-        // Adding details to event
+        })();
     }
 
     //Save a new full event (e.g a new worship event)
