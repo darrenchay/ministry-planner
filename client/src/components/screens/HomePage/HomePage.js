7f7e28b719eacacd26027b8c5da447bfe2c9a234
emile29 <emile.ltc@gmail.com>
2021-08-19 22:58:59 -0400

    refactor current styling to scss

--- client/src/components/screens/HomePage/HomePage.js
+++ client/src/components/screens/HomePage/HomePage.js
@@ -1,42 +1,11 @@
+import './HomePage.scss'
 import React from "react";
 import { Box } from "grommet";
-import { Typography, makeStyles, Button, Grid} from '@material-ui/core';
-
-
-const mystyle={
-  background: 'linear-gradient(#D83B1E 0%, #FE9969 100%)'
-}
-
-const useStyles = makeStyles((theme) => ({
-  mainVerse: {
-      flexGrow: 1,
-      fontWeight: "bold",
-      color: "white",
-      paddingTop: "50px",
-      borderRadius: "10px",
-  },
-  mainVerseRef: {
-    paddingBottom: "80px",
-    color: "white",
-  },
-  homeNavButtons: {
-    boxShadow: '6px 6px 4px rgba(0, 0, 0, 0.3)',
-    backgroundColor: 'white',
-    width: 200,
-    height: 250,
-    fontWeight: "bold",
-    color: "#D83B1E",
-    fontSize: 20,
-    padding: "12px",
-    borderRadius: "15px",
-  },
-}));
+import { Typography, Button, Grid } from '@material-ui/core';
 
 export default function HomePage() {
-  const classes = useStyles();
-
   return (
-    <div style={mystyle}>
+    <div className='home-page-wrapper'>
 
       <Box
         gridArea="main"
@@ -46,14 +15,14 @@ export default function HomePage() {
         style={{ margin: "auto" }}
       >
           <Typography 
-            className={classes.mainVerse} 
+            className='main-verse'
             variant="h4" 
             align="center" 
           >
             EQUIPPED FOR EVERY GOOD WORK
           </Typography>
           <Typography 
-            className={classes.mainVerseRef} 
+            className='main-verse-ref'
             align="right"
           >
             2 Tim 3:17
@@ -65,13 +34,13 @@ export default function HomePage() {
       justify='space-around'
       alignItems='center'
       >
-        <Button variant="contained" className={classes.homeNavButtons}>
+        <Button variant="contained" className='home-nav-buttons'>
           Planner
         </Button>
-        <Button variant="contained" className={classes.homeNavButtons}>
+        <Button variant="contained" className='home-nav-buttons'>
           Resources
         </Button>
-        <Button variant="contained" className={classes.homeNavButtons}>
+        <Button variant="contained" className='home-nav-buttons'>
           Team
         </Button>
       </Grid>
