1c6668fa7b03aad18c371bc5a539b0d5cb76aa71
Darren Chay <darrenchay@gmail.com>
2021-08-25 19:41:17 -0400

    added credential for cors

--- server/index.js
+++ server/index.js
@@ -5,8 +5,13 @@ import bodyParser from 'body-parser';
 import { init } from './routes';
 
 const app = express();
+const corsOptions ={
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
