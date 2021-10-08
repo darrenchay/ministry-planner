1bf18b0da3b6a9e15053fb93951943e6a8bd979e
Darren Chay <darrenchay@gmail.com>
2021-10-08 15:37:19 -0400

    finishing up

--- client/src/components/utils/Footer/Footer.js
+++ client/src/components/utils/Footer/Footer.js
@@ -4,30 +4,41 @@ import HomeIcon from '@material-ui/icons/Home';
 import PhoneIcon from '@material-ui/icons/Phone';
 import EmailIcon from '@material-ui/icons/Email';
 import LanguageIcon from '@material-ui/icons/Language';
+import {
+    Typography,
+    Divider
+} from '@material-ui/core';
 
 export default function Footer() {
-  return (
-    <div className="footer-wrapper">
-      <div className="info">
-        <HomeIcon className="icons" />
-        <div className="text">31 Magon Street, Plaine Verte, Port Louis, MAURITIUS</div>
-      </div>
+    return (
+        <>
+            <div className="footer-wrapper">
+                <Divider variant="middle" className="divider" />
+                <div className="info">
+                    <HomeIcon className="icons" />
+                    <div className="text">31 Magon Street, Plaine Verte, Port Louis, MAURITIUS</div>
+                </div>
 
-      <div className="info">
-        <PhoneIcon className="icons" />
-        <div className="text">(+230) 242 1120 / (+230) 242 1119</div>
-      </div>
+                <div className="info">
+                    <PhoneIcon className="icons" />
+                    <div className="text">(+230) 242 1120 / (+230) 242 1119</div>
+                </div>
 
-      <div className="info">
-        <LanguageIcon className="icons" />
-        <a href="https://stpaulmauritius.church">stpaulmauritius.church</a>
-      </div>
+                <div className="info">
+                    <LanguageIcon className="icons" />
+                    <a href="https://stpaulmauritius.church">stpaulmauritius.church</a>
+                </div>
 
-      <div className="info">
-        <EmailIcon className="icons" />
-        <div className="text">stpaulpv@gmail.com</div>
-      </div>
+                <div className="info">
+                    <EmailIcon className="icons" />
+                    <div className="text">stpaulpv@gmail.com</div>
+                </div>
 
-    </div>
-  );
+                <Typography variant="caption" color="inherit" className="copyright">
+                    © {new Date().getFullYear()} Wazaza Team
+                </Typography>
+
+            </div>
+        </>
+    );
 }
