befdea7ce4d777c3fcac8ba02640bbb8c63af738
Darren Chay <darrenchay@gmail.com>
2021-10-08 15:03:46 -0400

    uncommenting url

--- client/src/components/utils/Services/UsersAPI.js
+++ client/src/components/utils/Services/UsersAPI.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const baseURL = (process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_DEV_API_URL);
-const baseURL = "http://localhost:8080/api/"
+const baseURL = (process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PROD_API_URL : process.env.REACT_APP_DEV_API_URL);
 export const getUser = (id) => {
     return axios
         .get(baseURL + "users/" + id)
