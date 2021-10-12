3ead1cd184c8dae49397873ac77d0f88e15f3565
Darren Chay <darrenchay@gmail.com>
2021-10-12 02:48:07 -0400

    Revert "Merge branch 'master' of https://git.heroku.com/ministry-planner-server into add-worship-leader-role"
    This reverts commit aea203471e3e4038fcbf796e81f38ae0560cd00e, reversing
    changes made to a6c98bab562ed6c2dbf49c76d4f6033a849fd59e.

--- handlers/planner-handler.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import PlannerActions from './../actions/planner-page-actions';
-
-const router = express.Router();
-
-router.get('/getAll/:type', function(req, res) { PlannerActions.getAll(req, res) });
-
-export default router;
