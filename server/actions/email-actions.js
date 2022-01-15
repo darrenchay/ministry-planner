50092c8bd7b8e815bd1db8754b860c87c13ef87f
Darren Chay <darrenchay@gmail.com>
2022-01-15 18:21:46 -0500

    fixing stuff

--- server/actions/email-actions.js
+++ server/actions/email-actions.js
@@ -6,17 +6,15 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 class EmailActions {
     sendEmail(req, res) {
         var users = req.body.users;
-        console.log(users.length)
         for (var i = 0; i < users.length; i++) {
             var data = users[i];
             const msg = {
                 template_id: process.env.SENDGRID_TEMPLATE_ID,
-                from: data.sender.email,
                 personalizations: [{
                     to: { email: data.recipient.email },
                     dynamic_template_data: {
-                        Event_Name: data.eventName,
-                        Event_Date: data.eventDate,
+                        event_name: data.eventName,
+                        event_date: data.eventDate,
                         recipient_name: data.recipient.name,
                         sender_name: data.sender.name
                     },
@@ -25,11 +23,9 @@ class EmailActions {
 
             sendgrid
                 .send(msg)
-                .then(() => {
-                    console.log("sent to: " + data.recipient.name);
-                })
                 .catch((err) => {
                     res.status(400).send(err.message);
+                    return;
                 })
         }
         res.status(200).send("Emails sent successfully");
